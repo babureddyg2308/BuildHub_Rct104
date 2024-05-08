1191a6e4ab9b9100b2b5edf4276ba39bb07e89ea
Pratik Raj <kpratik071997@gmail.com>
2024-05-08 23:28:24 +0530

    day1 task added

--- BuildHub/src/components/Demo.jsx
+++ BuildHub/src/components/Demo.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Demo = () => {
   return (
     <div>
-      
+      <div>Hello Pratik</div>
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
