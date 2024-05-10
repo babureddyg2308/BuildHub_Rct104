71da37bf3f06d2af2c9f8f3e0720a8fef6519abf
Pratik Raj <kpratik071997@gmail.com>
2024-05-10 21:48:03 +0530

    Day3 code added

--- BuildHub/src/App.jsx
+++ BuildHub/src/App.jsx
@@ -1,10 +1,39 @@
+import React from "react";
 import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function Fitness() {
+  return <h2>Fitness Page</h2>;
+}
+
+function Sports() {
+  return <h2>Sports Page</h2>;
+}
+
+function Care() {
+  return <h2>Care Page</h2>;
+}
+
+function Mind() {
+  return <h2>Mind Page</h2>;
+}
+
+function Store() {
+  return <h2>Store Page</h2>;
+}
 
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-    </>
+      <Routes>
+        <Route path="/fitness" element={<Fitness />} />
+        <Route path="/sports" element={<Sports />} />
+        <Route path="/care" element={<Care />} />
+        <Route path="/mind" element={<Mind />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+    </Router>
   );
 }
 
--- BuildHub/src/components/NavBar.jsx
+++ BuildHub/src/components/NavBar.jsx
@@ -2,37 +2,33 @@ import React from "react";
 import "./index.css";
 import { FaUser, FaMapMarkerAlt } from "react-icons/fa";
 import BuildHubLogo from "../assets/BuildHub_logo.png";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="navbar">
-      <a href="/" className="logo-link">
+      <Link to="/" className="logo-link">
         <img src={BuildHubLogo} alt="logo" className="logo" />
-        {/* //  <span className="logo-text">BuildHub</span> */}
-      </a>
+      </Link>
 
-      <ul className="texts">
-        <a href="#fitness">FITNESS</a>
-
-        <a href="#sports">SPORTS</a>
-
-        <a href="#care">CARE</a>
-
-        <a href="#mind">MIND</a>
-
-        <a href="#store">STORE</a>
-      </ul>
+      <div className="texts">
+        <Link to="/fitness">FITNESS</Link>
+        <Link to="/sports">SPORTS</Link>
+        <Link to="/care">CARE</Link>
+        <Link to="/mind">MIND</Link>
+        <Link to="/store">STORE</Link>
+      </div>
 
       <div className="navbar-right">
-        <a href="#location" className="nav-link">
+        <Link to="/location" className="nav-link">
           <FaMapMarkerAlt /> CITY
-        </a>
-        <a href="#get-app" className="get-app-btn">
+        </Link>
+        <Link to="/get-app" className="get-app-btn">
           GET APP
-        </a>
-        <a href="#login" className="login-icon">
+        </Link>
+        <Link to="/login" className="login-icon">
           <FaUser />
-        </a>
+        </Link>
       </div>
     </div>
   );
