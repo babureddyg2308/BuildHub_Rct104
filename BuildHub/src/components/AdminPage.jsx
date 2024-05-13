e558a912510522b28bf2559cd70e7457ac84a0b8
babureddyg2308 <158761114+babureddyg2308@users.noreply.github.com>
2024-05-13 18:26:12 +0530

    added the css to  pages and corrected the code

---
+++ BuildHub/src/components/AdminPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Admin from '../components/Admin/Admin'
+
+const AdminPage = () => {
+  return (
+    <div>
+      <Admin/>
+
+    </div>
+  )
+}
+
+export default AdminPage
